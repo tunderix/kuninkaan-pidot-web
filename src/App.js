c7f3eafe4a7620d5290e124f7af302c3eff1bacf
Ioni <sami.repo@live.com>
2018-02-08 12:48:25 +0200

    ScrollBar Customization.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from "./Components/Main/";
 
+
 class App extends Component {
   render() {
+    
     return (
       <div className="App-Body">
-        <Main/>
+          <Main/>
       </div>
     );
   }
